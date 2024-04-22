4980b15a760e7960b9ffc865e4963ae2d06fd065
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-04-22 09:30:40 -0400

    feat(zsh): add togif alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -35,6 +35,11 @@ gdiff() {
 	fi
 }
 
+# togif source.mp4 output.gif
+togif() {
+  ffmpeg -i $1 -filter_complex "[0:v] fps=12,split [a][b];[a] palettegen [p];[b][p] paletteuse" $2
+}
+
 # npm aliases
 
 alias ni="npm install"
