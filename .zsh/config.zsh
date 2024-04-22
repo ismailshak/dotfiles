871208872528ec282d660337899befdcc03874a5
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-04-22 17:58:17 -0400

    feat(zsh): add tools switch to PATH

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -22,6 +22,9 @@ eval "$(zoxide init zsh)"
 # Add local node_modules to PATH
 export PATH="node_modules/.bin:$PATH"
 
+# Add global tools OPAM switch to PATH (OCaml)
+[ -d "$HOME/.opam/tools/bin" ] && export PATH="$PATH:$HOME/.opam/tools/bin"
+
 # main aliases
 source ~/.zsh/aliases.zsh
 
