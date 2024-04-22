b4ac70a154be6a83c378fa459022801f334d5f1c
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-04-22 17:57:28 -0400

    feat(zsh): move omz config over

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,3 +1,24 @@
+# oh-my-zsh configuration
+#------------------------
+
+export ZSH="$HOME/.oh-my-zsh"
+
+# make omz history wrapper output timestamps
+HIST_STAMPS="yyyy.mm.dd"
+
+plugins=(git)
+
+# Load oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+
+#------------------------
+
+# Initialize starship prompt
+eval "$(starship init zsh)"
+
+# Initialize zoxide
+eval "$(zoxide init zsh)"
+
 # Add local node_modules to PATH
 export PATH="node_modules/.bin:$PATH"
 
@@ -6,10 +27,4 @@ source ~/.zsh/aliases.zsh
 
 # fzf aliases
 source ~/.zsh/fzf_aliases.zsh
-
-# Initialize starship prompt
-eval "$(starship init zsh)"
-
-# Initialize zoxide
-eval "$(zoxide init zsh)"
 
