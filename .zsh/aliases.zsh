e157102125ac8806fbabde4be7af9171a6121953
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-05-24 08:44:48 -0400

    feat(zsh): add diff-in-nvim alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -15,24 +15,17 @@ alias c="clear"
 delete_all_dirs() { find . -name "$1" -type d -print -prune -exec rm -rf '{}' \; }
 delete_all_files() { find . -name "$1" -type f -print -prune -exec rm -f '{}' \; }
 killport() { kill -9 "$(lsof -i:"$1" -t)" }
-cdiff() { code --diff "$1" "$2" }
-# togif source.mp4 output.gif
 togif() { ffmpeg -i $1 -filter_complex "[0:v] fps=12,split [a][b];[a] palettegen [p];[b][p] paletteuse" $2 }
+
+cdiff() { code --diff "$1" "$2" }
+ndiff() { nvim -d "$1" "$2" }
+gdiff() { git diff --color --no-index "$1" "$2" }
 
 # git aliases (most aliases are in ~/.gitconfig)
 
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
 alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
 alias gco="git checkout"
-
-gdiff() {
-	# If no args, use default diff command
-	if [ $# -eq 0 ]; then
-		git diff
-	else
-		git diff --color --no-index "$1" "$2"
-	fi
-}
 
 # npm aliases
 
