f23bfe63def9a2bf23afbadb6a42847010264aa1
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-08-29 09:42:05 -0400

    feat(yabai): update window padding value

--- .config/yabai/toggle-padding.sh
+++ .config/yabai/toggle-padding.sh
@@ -9,18 +9,18 @@ TOP_PADDING=$(yabai -m config --space "$SPACE" top_padding)
 
 # If the current padding is 10, then set it to 20
 if [[ $TOP_PADDING -eq 0 ]]; then
-    yabai -m config --space "$SPACE" top_padding 20
-    yabai -m config --space "$SPACE" bottom_padding 20
-    yabai -m config --space "$SPACE" left_padding 280
-    yabai -m config --space "$SPACE" right_padding 280
-    yabai -m config --space "$SPACE" window_gap 20
+	yabai -m config --space "$SPACE" top_padding 20
+	yabai -m config --space "$SPACE" bottom_padding 20
+	yabai -m config --space "$SPACE" left_padding 200
+	yabai -m config --space "$SPACE" right_padding 200
+	yabai -m config --space "$SPACE" window_gap 20
 fi
 
 # If the current padding is 20, then set it to 0
 if [[ $TOP_PADDING -eq 20 ]]; then
-    yabai -m config --space "$SPACE" top_padding 0
-    yabai -m config --space "$SPACE" bottom_padding 0
-    yabai -m config --space "$SPACE" left_padding 0
-    yabai -m config --space "$SPACE" right_padding 0
-    yabai -m config --space "$SPACE" window_gap 0
+	yabai -m config --space "$SPACE" top_padding 0
+	yabai -m config --space "$SPACE" bottom_padding 0
+	yabai -m config --space "$SPACE" left_padding 0
+	yabai -m config --space "$SPACE" right_padding 0
+	yabai -m config --space "$SPACE" window_gap 0
 fi
